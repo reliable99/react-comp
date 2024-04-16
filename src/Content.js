a62228a3984a0a35e89e011d4147ae7ad320ba9e
reliable99 <abdulmuhminsodiq@gmail.com>
2024-04-16 15:30:21 +0100

    update

--- src/Content.js
+++ src/Content.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import { useState } from "react";
 
 const Content = () => {
 
+  const [name, setName] = useState("kunle")
+  const [count, setCount] = useState(0)
     const handleNameChange = () => {
         const names = ["bola", "kunle", "ayo"];
         const randomIndex = Math.floor(Math.random() * 3);
-        return names[randomIndex];
+        setName(names[randomIndex]);
       }
+
+      const handleClick = () => {
+        setCount(count+1)
+        setCount(count+1)
+        console.log(count)
+      }
+
+      const handleClick2 = (name) => {
+        console.log(count)
+      }
+
+    
   return (
     <main>
-        <p>
-            Hello {handleNameChange()}!
+        <p onDoubleClick={handleClick}>
+            Hello {name}!
         </p>
+        <button onClick={handleNameChange}>change name  </button>
+
+        <button onClick={handleClick}>click it  </button>
+
+        <button onClick={handleClick2}>click it  </button>
     </main>
   )
 }
