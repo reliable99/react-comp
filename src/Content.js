c55b572ed193f8d4a4b5c504a5e810b306fd6980
reliable99 <abdulmuhminsodiq@gmail.com>
2024-04-17 13:53:32 +0100

    update

--- src/Content.js
+++ src/Content.js
@@ -1,38 +1,63 @@
 import { useState } from "react";
+import {FaTrashAlt} from "react-icons/fa";
 
 const Content = () => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
 
-  const [name, setName] = useState("kunle")
-  const [count, setCount] = useState(0)
-    const handleNameChange = () => {
-        const names = ["bola", "kunle", "ayo"];
-        const randomIndex = Math.floor(Math.random() * 3);
-        setName(names[randomIndex]);
-      }
+ 
 
-      const handleClick = () => {
-        setCount(count+1)
-        setCount(count+1)
-        console.log(count)
-      }
+  const handleCheck = (id) => {
+    const listItems = items.map ((item) => item.id === id ? {...item, checked: !item.checked} : item );
+    setItems(listItems)
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  }
 
-      const handleClick2 = (name) => {
-        console.log(count)
-      }
+  const handleDelete =(id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  }
 
-    
-  return (
+  return( 
     <main>
-        <p onDoubleClick={handleClick}>
-            Hello {name}!
-        </p>
-        <button onClick={handleNameChange}>change name  </button>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+              <input
+              onChange={() => handleCheck(item.id)}
+              type="checkbox"
+              checked={item.checked}
+              />
+              <label
+              style={(item.checked) ? {textDecoration: "line-through"} : null }
+                onDoubleClick={() => handleCheck(item.id)}
+              >{item.item}</label>
+              <FaTrashAlt
+              onClick={() =>handleDelete(item.id)} 
+              role="button"
+               tabIndex="0"
+                />
+          </li>
+        ))}
+      </ul>
 
-        <button onClick={handleClick}>click it  </button>
-
-        <button onClick={handleClick2}>click it  </button>
     </main>
   )
-}
+};
 
-export default Content
+export default Content;
