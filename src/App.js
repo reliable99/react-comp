91e27b4ac79473f7d6f488ac1214cac4e639b4a2
reliable99 <abdulmuhminsodiq@gmail.com>
2024-04-20 19:37:30 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Content from './Content';
 import Footer from './Footer';
 import Header from './Header';
+import AddItem from './AddItem';
 
 function App() {
   const [items, setItems] = useState([
@@ -23,6 +24,8 @@ function App() {
     }
 ]);
 
+const [newItem, setNewItem] = ('')
+
 const handleCheck = (id) => {
   const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
   setItems(listItems);
@@ -34,10 +37,19 @@ const handleDelete = (id) => {
   setItems(listItems);
   localStorage.setItem('shoppinglist', JSON.stringify(listItems));
 }
+
+const handleSubmit = (e) => {
+  console.log('submitted')
+}
   
   return (
     <div className='App'>
       <Header title = "Groseries List"/>
+      <AddItem
+      newItem={newItem}
+      setItem={setNewItem}
+      handleSubmit={handleSubmit}
+      />
       <Content
        items={items}
        handleCheck={handleCheck}
